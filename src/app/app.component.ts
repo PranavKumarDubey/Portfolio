a2e555bdee6291ca29198aa622a1b6284b5248c3
Pranav Kumar Dubey <pranavd709@gmail.com>
2025-03-18 12:57:16 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AnimationComponent } from './animation/animation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AnimationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'PranavKumarDubey';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+    path:"",
+    component: HomeComponent
+},
+{
+    path:"home",
+    component: HomeComponent
+},
+{
+    path:"about",
+    component: AboutComponent
+},
+{
+    path:"project",
+    component: ProjectsComponent
+},
+{
+    path:"skills",
+    component: SkillsComponent
+},
+{
+    path:"contact",
+    component: ContactComponent
+}
+
+];
